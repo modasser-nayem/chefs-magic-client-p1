e45e7cb1564993ad05b07f390f49d94d6ca5876c
developer-nayem <mdalimodassernayem@gmail.com>
2023-05-02 09:16:16 +0600

    create folder structure

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+   return (
+      <div>
+         <Navbar />
+         <div>
+            <Outlet />
+         </div>
+         <Footer />
+      </div>
+   );
+};
+
+export default MainLayout;
